be1c62ea9654d6b5c5b3efbdd6d53aecac6f712f
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2024-01-27 12:49:52 -0600

    Update resn.py

--- resn.py
+++ resn.py
@@ -1,36 +1,37 @@
 import streamlit as st
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
-from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
 import numpy as np
-import os
 
-# Obtener la ruta del directorio actual
-current_directory = os.path.dirname(os.path.realpath(__file__))
+# Cargar el modelo ResNet50 preentrenado
+model = ResNet50(weights='imagenet')
 
-
-# Combinar la ruta del directorio actual con el nombre del modelo
-model_path = os.path.join(current_directory, 'resnet50_model.h5')
-
-# Cargar el modelo previamente entrenado
-model = load_model(model_path)
-
-st.title('Detector de Insectos con ResNet50')
-
-# Cargar la imagen de entrada
-uploaded_file = st.file_uploader("Cargar una imagen", type="jpg")
-
-if uploaded_file is not None:
-    # Preprocesar la imagen para que coincida con el formato que ResNet50 espera
-    img = image.load_img(uploaded_file, target_size=(224, 224))
+def predict_image(img_path):
+    # Cargar la imagen y preprocesarla para hacerla compatible con ResNet50
+    img = image.load_img(img_path, target_size=(224, 224))
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
     img_array = preprocess_input(img_array)
 
-    # Realizar la inferencia
+    # Realizar la predicción
     predictions = model.predict(img_array)
+    decoded_predictions = decode_predictions(predictions, top=3)[0]
 
-    # Mostrar los resultados
-    st.image(img, caption='Imagen de entrada', use_column_width=True)
-    st.write('Predicciones:')
-    st.write(predictions)
+    return decoded_predictions
+
+st.title("Aplicación de Detección de Objetos con ResNet50")
+
+uploaded_file = st.file_uploader("Cargar una imagen...", type="jpg")
+
+if uploaded_file is not None:
+    st.image(uploaded_file, caption='Imagen de entrada.', use_column_width=True)
+    st.write("")
+    st.write("Clasificando...")
+
+    # Hacer la predicción
+    predictions = predict_image(uploaded_file)
+    
+    st.write(f"Predicciones (Top 3):")
+    for i, (imagenet_id, label, score) in enumerate(predictions):
+        st.write(f"{i + 1}: {label} ({score:.2f})")
+
